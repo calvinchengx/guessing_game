f9ed963f8b52ba1b3f929ee1b32e25562f735094
Calvin Cheng <linchuan.cheng@gmail.com>
2018-10-21 10:04:30 +0800

    loop match and break, like switch case

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,6 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
-
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
@@ -23,9 +21,18 @@ fn main() {
 
     println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+
+        println!("Please input your guess.");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+
+
 }
